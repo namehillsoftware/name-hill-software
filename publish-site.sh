b63c807cbab86cc1211fcf39779f8792d7bf9ebc
David <dvedvick@gmail.com>
2024-07-14 14:49:41 -0500

    Update publish folder

--- publish-site.sh
+++ publish-site.sh
@@ -16,7 +16,7 @@ rsync -avzh \
   --include='*/' \
   --exclude='*' \
   --prune-empty-dirs \
-  ./build/public/ "$SSH_USERNAME"@"$SSH_HOST":/home/protected/app/public
+  ./build/public/ "$SSH_USERNAME"@"$SSH_HOST":/home/public
 
 EXIT_CODE=${PIPESTATUS[0]}
 
